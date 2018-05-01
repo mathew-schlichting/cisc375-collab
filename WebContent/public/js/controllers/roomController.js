b5317bc22bf9b7090f584a0d337c14ea1a88c918
Emily Meuer <emily.meuer@gmail.com>
2018-05-01 15:39:17 -0500

    Starting on client.

--- WebContent/public/js/controllers/roomController.js
+++ WebContent/public/js/controllers/roomController.js
@@ -3,36 +3,40 @@
  */
 function roomControllerFunction($scope, $stateParams) {
 
-    $scope.roomid = '0001';
-    $scope.connection = null;
-    $scope.meesage = '';
+	$scope.roomid = '0001';
+	$scope.connection = null;
+	$scope.meesage = '';
+
+	$scope.localVideo;
+	$scope.remoteVideo;
+	$scope.peerConnection;
 
 
-    $scope.init = function(){
-        console.log('room');
+	$scope.init = function() {
+		console.log('room');
 
-        $scope.roomid = $stateParams.roomid;
+		$scope.roomid = $stateParams.roomid;
 
-        $scope.connection = new WebSocket('ws://' + window.location.hostname + ':8018');
+		$scope.connection = new WebSocket('ws://' + window.location.hostname + ':8018');
 
-        $scope.connection.onopen = (event) => {
-            console.log('on open');
-            $scope.connection.send("something");
-        };
-        $scope.connection.onmessage = (message)=> {
-              console.log(message);
-        };
-
-
-
-    };
+		$scope.connection.onopen = (event) => {
+			console.log('on open');
+			$scope.connection.send("something");
+		};
+		$scope.connection.onmessage = (message) => {
+			console.log(message);
+		};
 
 
 
-    $scope.sendMessage = function(){
-        $scope.connection.send("something");
+	};
 
-    }
+
+
+	$scope.sendMessage = function() {
+		$scope.connection.send("something");
+
+	}
 
 
 
