a2dd3672033aaa8acce64ba6521d424a197cd067
Emily Meuer <emily.meuer@gmail.com>
2018-05-08 14:58:19 -0500

    Using promises for createAnswer and createOffer

--- Source/server.js
+++ Source/server.js
@@ -124,6 +124,7 @@ function getUserList(roomid) {
 }
 
 function joinRoom(roomid, username) {
+    console.log("joinRoom: username = " + username)
 	rooms[roomid].users.push(username);
 	people[username].room = roomid;
 	broadcastInRoom(roomid, 'user_joined', 'server', {
@@ -168,7 +169,42 @@ function broadcastInLobby(type, from, data) {
 	}
 }
 
+// TODO - current problem is that it's not recognizing the room number
+// (even when it prints as being there) -- maybe it's happening too quickly?
 function broadcastInRoom(roomid, type, from, data) {
+    /*
+    var sendToPerson    = true;
+    for (var p in people) {
+        if (people[p]) {    sendToPerson    = sendToPerson && people[p]; }
+        else {
+            console.log(p + ": Problem = people[p]:");
+            //console.log(people[p]);
+        }
+        if (people[p].client !== null) {    sendToPerson    = sendToPerson && people[p].client !== null; }
+        else {
+            console.log(p + ": Problem = people[p].client !== null:");
+            //console.log(people[p].client !== null);
+        }
+        if (people[p].client.connected) {    sendToPerson    = sendToPerson && people[p].client.connected; }
+        else {
+            console.log(p + ": Problem = people[p].client.connected:");
+            //console.log(people[p].client.connected);
+        }
+        var peopleRoom  = people[p].room;
+        console.log("Comparing id's: " + (peopleRoom === roomid));
+        if (peopleRoom === roomid) {    sendToPerson    = sendToPerson && peopleRoom === roomid; }
+        else {
+            console.log(p + ": people[p].room = " + peopleRoom + "; roomid = " + roomid);
+        }
+
+        console.log(people[p].client, type, from, data, sendToPerson);
+
+        if(people[p] && (people[p].client !== null) && (people[p].client.connected) && (people[p].room === roomid)) {
+            sendData(people[p].client, type, from, data);
+        }
+    }
+    */
+
 	for (var p in people) {
 		if (people[p] && people[p].client !== null && people[p].client.connected && people[p].room === roomid) {
 			sendData(people[p].client, type, from, data);
@@ -220,7 +256,7 @@ function initSocketIO() {
 		});
 
 		client.on('join_room', (message) => {
-			console.log('Received join_room');
+			console.log('Received join_room: roomid = ' + message.data.roomid + '; from = ' + message.from);
 			joinRoom(message.data.roomid, message.from);
 		});
 
@@ -246,17 +282,17 @@ function initSocketIO() {
             console.log('Received leave_room');
             leaveRoom(message.from);
         });
-		
+
 		client.on('rtc', (message) => {
 			console.log('Received rtc');
 			broadcastInRoom(people[message.from].room, 'rtc', message.from, message.data);
 		});
-		
+
 		client.on('start_call', (message) => {
 			broadcastInRoom(people[message.from].room, 'start_call', message.from, message.data);
 		});
-		
-        
+
+
         client.emit('init_client', createMessage('server', {id: currentId}));
     });
 
